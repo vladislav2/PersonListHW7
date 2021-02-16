12c0142d8e8a9a4e3980a744d52b513b8de001bb
vladislav2 <etter.id75@gmail.com>
2021-02-16 11:51:42 +0200

    Initial Commit

---
+++ PersonsListHW7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonsListHW7
+//
+//  Created by user on 16.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
